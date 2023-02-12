2ae0eaa1c15566d59f4e2e2ce46e1c0542c61e0a
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-02-12 20:59:42 +0700

    Ví dụ về bind trong DOM Event

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 // fn.bind() trong javascript
-this.firstName = "Minh";
-this.lastName = "Thu";
 
 const teacher = {
   firstName: "Minh",
   lastName: "Thao",
-  getFullName(fullName1, fullName2) {
-    console.log(fullName1, fullName2);
-    return `${this.firstName} ${this.lastName}`;
+  getFullName() {
+    console.log(`${this.firstName} ${this.lastName}`);
   },
 };
 
-const getTeacherName = teacher.getFullName.bind(teacher, "data1", "data2"); // dữ liệu này sẽ override bên dưới.
-console.log(getTeacherName("Full Name", "Hello"));
+// Ví dụ về bind trong DOM event
+const btnElement = document.querySelector("button");
+btnElement.onclick = teacher.getFullName.bind(teacher);
