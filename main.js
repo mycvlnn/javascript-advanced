68cdc6f8505d09572509dc6d563e935935b0219d
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-02-12 21:49:54 +0700

    bind method practice

--- main.js
+++ main.js
@@ -1,13 +1,54 @@
-// fn.bind() trong javascript
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const carListElement = $("#car-list");
+const btnAdd = $("button");
+const inputElemnet = $("input");
 
-const teacher = {
-  firstName: "Minh",
-  lastName: "Thao",
-  getFullName() {
-    console.log(`${this.firstName} ${this.lastName}`);
-  },
-};
+const app = (() => {
+  return {
+    cars: [],
+    add(carName) {
+      this.cars.push(carName);
+    },
+    deleteCar(index) {
+      this.cars.splice(index, 1);
+    },
+    render() {
+      const html = this.cars
+        .map((car, index) => {
+          return `<li>
+                    ${car}
+                    <span class="delete" data-index=${index}>&times</span>
+                </li>`;
+        })
+        .join("");
 
-// Ví dụ về bind trong DOM event
-const btnElement = document.querySelector("button");
-btnElement.onclick = teacher.getFullName.bind(teacher);
+      carListElement.innerHTML = html;
+    },
+    handleDelete(e) {
+      const deleteElement = e.target.closest(".delete");
+      if (deleteElement) {
+        const index = deleteElement.dataset.index;
+        this.deleteCar(index);
+        this.render();
+      }
+    },
+    init() {
+      // Lắng nghe sự kiện
+      // 1. add
+      btnAdd.onclick = () => {
+        const valueInput = inputElemnet.value;
+        console.log("hihih");
+        this.add(valueInput);
+        this.render();
+      };
+
+      // 2. delete
+      carListElement.onclick = this.handleDelete.bind(this);
+
+      this.render();
+    },
+  };
+})();
+
+app.init();
