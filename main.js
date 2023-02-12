f14d75b6b495c184507a482fe76ee6cd55ebe2b3
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-02-12 17:02:50 +0700

    Primitive types and References types

--- main.js
+++ main.js
@@ -1,42 +1,134 @@
-// Học về strict mode in javascript
+// Học về các kiểu dữ liệu trong javascript
 
-"use strict";
+// 1. Kiểu dữ liệu nguyên thuỷ ( primitive data types )
+let a = 1;
 
-// fullName = "Chris"; // Tạo ra một biến fullName ở phạm vi global
+let b = a;
 
-// function testFunc() {
-//   age = 18; // Tạo ra một biến age ở phạm vi global.
-// }
+a = 2;
 
-// testFunc();
+console.log(b); // 1
+console.log(a); // 2
 
-// console.log(fullName);
+// Kểu dữ liệu tham chiếu (reference types)
 
-// console.log(age); // 18
+const car = {
+  name: "Mercedes",
+};
 
-// 1. Báo lỗi khi gán lại giá trị cho thuộc tính có writable: false
+const myCar = car;
 
-// const students = Object.freeze({
-//   name: "Chris",
-// });
+car.name = "VinFast";
 
-// students.name = "hello"; // error
+console.log(myCar);
 
-// console.log(students.name);
+// Điều gì sẽ xảy ra với mã như sau:
+let x = {
+  name: "Chris",
+};
 
-// 2.
-// function sum(a, a) {
-//   return a + a;
-// }
+x.name = "ABC";
 
-// sum(6, 9); // error
+x = {
+  name: "Hello",
+};
 
-// 3.
+console.log({ x }); // {name: 'Hello'}
 
-// {
-//   function myFn() {
-//     console.log("hello");
-//   }
-// }
+const student = {
+  name: "Chris",
+  profile: {
+    firstName: "Hello",
+    lastName: "My friend",
+  },
+};
 
-// myFn(); // error
+const myProfile = student.profile;
+student.profile.firstName = "Chris";
+console.log({ myProfile });
+
+// Data types with function
+
+// 1. Primitive
+function sum(a, b) {
+  // Bản thân nó sẽ chạy ngầm lệnh sau:
+  // let a; let b
+  // a = c
+  // b = d
+
+  a = 0;
+  b = 0;
+
+  console.log(a, b); // 0, 0
+}
+
+const c = 1;
+const d = 2;
+
+sum(c, d);
+
+console.log(c, d); // 1, 2
+
+// 2. References
+
+function callback(obj) {
+  obj.name = "Mercedes";
+
+  console.log(obj);
+}
+
+const y = {
+  name: "VS",
+};
+
+callback(y);
+
+console.log({ y });
+
+// clone shadow
+
+const n1 = {
+  name: "xx",
+  child: {
+    age: 8,
+  },
+};
+
+const n2 = { ...n1 };
+
+n2.child.age = 20;
+console.log({ n2, n1 }); // Kết quả giống nhau
+
+// clone deep
+const n3 = {
+  name: "xx",
+  child: {
+    age: 8,
+  },
+};
+
+const n4 = JSON.parse(JSON.stringify(n3));
+
+n4.child.age = 20;
+
+console.log(n3); // age = 8
+console.log(n4); // age = 20
+
+// So sánh 2 đối tượng
+
+const data1 = {
+  name: "a",
+};
+
+const data2 = {
+  name: "a",
+};
+
+console.log(data1 === data2); // false
+
+const x1 = {
+  a: "a",
+};
+
+const x2 = x1;
+console.log(x2 === x1); // true
