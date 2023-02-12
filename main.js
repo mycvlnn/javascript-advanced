59c0b3e51eb54dfd4c6e3e50626f9bf033fd74a7
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-02-12 19:47:26 +0700

    this đối với dom event

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-function Car(name, price, color) {
-  this.name = name;
-  this.price = price;
-  this.color = color;
-  this.run = function () {
-    console.log("running...", this); // this  nó sẽ là đối tượng mà nó gọi đến phương thức này.
-  };
-}
-
-const mercedes = new Car("Mercedes", 1000, "black");
-
-mercedes.run();
+const btnElement = document.querySelector("button");
+btnElement.onclick = function () {
+  console.log(this); // chính là btnElement
+};
