a1bde6a755e91fa1a7ef5c27ce5c667a1f38aca5
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-02-12 15:12:08 +0700

    strict mode

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
-// Học về hoisting in javascript
+// Học về strict mode in javascript
 
-// 1.  Đối với từ khoá var
-console.log(age); // undefined
-var age = 18;
-console.log(age); // 18
+"use strict";
 
-// 2. Đối với từ khoá function
+// fullName = "Chris"; // Tạo ra một biến fullName ở phạm vi global
 
-console.log(sum(1, 2)); // result: 3
+// function testFunc() {
+//   age = 18; // Tạo ra một biến age ở phạm vi global.
+// }
 
-function sum(a, b) {
-  return a + b;
-}
+// testFunc();
+
+// console.log(fullName);
+
+// console.log(age); // 18
+
+// 1. Báo lỗi khi gán lại giá trị cho thuộc tính có writable: false
+
+// const students = Object.freeze({
+//   name: "Chris",
+// });
+
+// students.name = "hello"; // error
+
+// console.log(students.name);
+
+// 2.
+// function sum(a, a) {
+//   return a + a;
+// }
+
+// sum(6, 9); // error
+
+// 3.
+
+// {
+//   function myFn() {
+//     console.log("hello");
+//   }
+// }
+
+// myFn(); // error
