ec3756820f8327bf95e68466a9fa645752336218
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-02-12 20:16:00 +0700

    Ví dụ về bind()

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-const btnElement = document.querySelector("button");
-btnElement.onclick = function () {
-  console.log(this); // chính là btnElement
+// fn.bind() trong javascript
+this.firstName = "Minh";
+this.lastName = "Thu";
+
+const teacher = {
+  firstName: "Minh",
+  lastName: "Thao",
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
 };
+
+// case 1:
+console.log(teacher.getFullName()); // Minh thảo
+
+// case 2:
+const getTeacherName = teacher.getFullName.bind(teacher);
+console.log(getTeacherName()); // Minh Thảo
+
+console.log(teacher.getFullName === getTeacherName); // return false
