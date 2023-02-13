176922fca77a0234ac801f972901e3d3c82c206f
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-02-13 10:26:49 +0700

    Thêm ví dụ về arguments (borrowing function)

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 // call()  method
 
-function Animal(name, weight) {
-  this.name = name;
-  this.weight = weight;
+function logger() {
+  const newArr = Array.prototype.slice.call(arguments); // Sử dụng để convert sang mảng.
+
+  console.log({ newArr });
 }
 
-function Chicken(name, weight, legs) {
-  Animal.call(this, name, weight); // kế thừa lại được thuộc tính của Animal
-  this.legs = legs;
-}
-
-const gaMai = new Chicken("Ga mai mo", 2, 2);
-console.log(gaMai);
+logger(1, 2, 3, 4);
